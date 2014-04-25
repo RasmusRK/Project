36b123971a84abc01ac67e8adeeea848a77c45d9
martinsoender <wxk220@ku.dk>
2014-04-25 11:27:44 +0200

    Sat index filen pænt op og rettet lidt.

--- web/index.php
+++ web/index.php
@@ -1,60 +1,51 @@
 <?php
 include_once 'includes/db_connect.php';
 include_once 'includes/functions.php';
- 
+
 sec_session_start();
- 
+
 if (login_check($mysqli) == true) {
     $logged = 'in';
 } else {
     $logged = 'out';
 }
 ?>
+
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Secure Login: Log In</title>
-        <link rel="stylesheet" href="css/pure.css" />
-        <script type="text/JavaScript" src="js/sha512.js"></script> 
-        <script type="text/JavaScript" src="js/forms.js"></script> 
-    </head>
-    <body background = "css/test.png">
+<head>
+    <title>Secure Login: Log In</title>
+    <link rel="stylesheet" href="css/pure.css" />
+    <script type="text/JavaScript" src="js/sha512.js"></script> 
+    <script type="text/JavaScript" src="js/forms.js"></script> 
+</head>
 
-        <div id = "login">
+<body background = "css/test.png">
+    <div id = "login">
         <?php
         if (isset($_GET['error'])) {
             echo '<p class="error">Error Logging In!</p>';
         }
         ?> 
         <form class = "boxCont" action="includes/process_login.php" method="post" name="login_form">                      
-                    <div>
-                    <label for= "userName"> Email </label>
-                    <input id = "userName" type="text" name="email" />
-                     </div>
+            <div>
+                <label for="userName"> Email </label>
+                <input id ="userName" type="text" name="email" />
+            </div>
+            <div>
+               <label for="password"> Kodeord </label>
+               <input type="password" name="password" id="password"/>
+           </div>
 
-                     <div>
-                     <label for "password"> Kodeord </label>
-                     <input type="password" 
-                             name="password" 
-                             id="password"/>
-                    </div>
-
-            <input class = "btn left"
-                   type="button" 
-                   value="Login" 
-                   onclick="formhash(this.form, this.form.password);" /> 
-            <a href = "register.php">
-            <input class = "btn right"
-                   type ="button"
-                   value ="Opret bruger" />
-                   </a>
+           <input class="btn left" type="button" value="Login" onclick="formhash(this.form, this.form.password);" /> 
+           <a href = "register.php"><input class="btn right" type="button" value="Opret bruger" /></a>
     </div>
+
     <!-- skal muligvis implementeres senere
         </form>
         <p>If you don't have a login, please <a href="register.php">register</a></p>
         <p>If you are done, please <a href="includes/logout.php">log out</a>.</p>
         <p>You are currently logged <?php echo $logged ?>.</p>
-    
     -->
-    </body>
+</body>
 </html>
