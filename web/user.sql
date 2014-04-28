48cd9e81b224f95d4248056e3ea402dd489933e3
TheNiceOne <henrik-thomsen@live.dk>
2014-04-28 22:17:03 +0200

    user.sql updated

--- web/user.sql
+++ web/user.sql
@@ -1,18 +1,4 @@
-create table user(
-	name varchar(255),
-	id varchar(255) PRIMARY KEY,
-	email varchar(255),
-	password varchar(255))
-
-INSERT INTO user (
-`Name` ,`email` , `id` ,`password`
-)
-VALUES (
-'ole',  'ole@ole.dk',  '1',  'ole'
-);
-
-
-CREATE TABLE  `projekt`.`members` (
+CREATE TABLE  `projekt`.`users` (
  `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
  `username` VARCHAR( 30 ) NOT NULL ,
  `email` VARCHAR( 50 ) NOT NULL ,
