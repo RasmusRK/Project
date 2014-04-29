8e07e54245597636c8b18799ef01383ab9eabd12
RasmusRK <rasmus.klitgaard.rk@gmail.com>
2014-04-29 10:51:10 +0200

    added admin to sql users

--- web/user.sql
+++ web/user.sql
@@ -17,5 +17,16 @@ CREATE TABLE  `projekt`.`members` (
  `username` VARCHAR( 30 ) NOT NULL ,
  `email` VARCHAR( 50 ) NOT NULL ,
  `password` CHAR( 128 ) NOT NULL ,
- `salt` CHAR( 128 ) NOT NULL
+ `salt` CHAR( 128 ) NOT NULL,
+ 'admin' TINYINT(1) NOT NULL
 ) ENGINE = INNODB
+
+
+CREATE TABLE  `projekt`.`login_attempts` (
+ `user_id` INT( 11 ) NOT NULL ,
+ `time` VARCHAR( 30 ) NOT NULL
+) ENGINE = INNODB
+
+INSERT INTO `projekt`.`users` VALUES(1, 'test_user', 'test@example.com',
+'00807432eae173f652f2064bdca1b61b290b52d40e429a7d295d76a71084aa96c0233b82f1feac45529e0726559645acaed6f3ae58a286b9f075916ebf66cacc',
+'f9aab579fc1b41ed0c44fe4ecdbfcdb4cb99b9023abb241a6db833288f4eea3c02f76e0d35204a8695077dcf81932aa59006423976224be0390395bae152d4ef');
