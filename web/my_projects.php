c7f674916e063a62fc94167120a006433284a63d
martinsoender <wxk220@ku.dk>
2014-04-30 12:02:30 +0200

    added files

---
+++ web/my_projects.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="A layout example with a side menu that hides on mobile, just like the Pure website.">
+    <title>Projects</title>
+<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.4.2/pure.css">
+
+    <!--[if lte IE 8]>
+        <link rel="stylesheet" href="css/layouts/side-menu-old-ie.css">
+    <![endif]-->
+    <!--[if gt IE 8]><!-->
+        <link rel="stylesheet" href="css/layouts/side-menu.css">
+    <!--<![endif]-->
+  </head>
+
+<body>
+
+<div id="layout">
+    <!-- Menu toggle -->
+    <a href="main.php" id="menuLink" class="menu-link">
+        <!-- Hamburger icon -->
+        <span></span>
+    </a>
+
+    <div id="menu">
+        <div class="pure-menu pure-menu-open">
+            <a class="pure-menu-heading" href="#">Test</a>
+
+            <ul>
+                <li><a href="my_projects.php">Mine projekter</a></li>
+                <li><a href="all_projects.php">Alle projekter</a></li>
+                <li><a href="history.php">Min historik</a></li>
+                <li><a href="contact.php">Kontakt</a></li>
+                <br><br>
+                <li>Logget ind som: <?php echo $_SESSION['username'];?></li>
+                <li>Du har bruger id: <?php echo $_SESSION['user_id'];?></li>
+                <li><a href="includes/logout.php">Log ud</a></li>
+            </ul>
+        </div>
+    </div>
+
+    <div id="main">
+        <div class="header">
+            <h1>Mine projekter</h1>
+            <h2>Liste af projekter du har arbejdet på</h2>
+        </div>
+
+        <div class="content">
+            <h2 class="content-subhead">Titel</h2>
+            <p>
+                Tekst
+            </p>
+        </div>
+    </div>
+</div>
+
+<script src="js/ui.js"></script>
+</body>
+</html>
