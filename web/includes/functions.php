7e6a04a5edbfcb281f011a1d0c480a6cc1057761
RasmusRK <rasmus.klitgaard.rk@gmail.com>
2014-04-30 14:33:23 +0200

    minor changes

--- web/includes/functions.php
+++ web/includes/functions.php
@@ -226,38 +226,6 @@ function euroDate($date){
 
 function show_projects($mysqli){
 
-    echo"<table class='pure-table pure-table-striped'>
-    <thead>
-    <tr>
-    <th>projekt id</th>
-    <th>projekt navn</th>
-    <th>kategori</th>
-    <th>start dato</th>
-    <th>slut dato</th>
-    </tr>
-    </thead>";
-
-    foreach($mysqli->query("SELECT projekt_id, projekt_name, kategori, start_date, end_date FROM projekt") as $row) {
-        $projekt_id = $row[0];
-        $projekt_name = $row[1];
-        $kategori = $row[2];
-        $start_date = $row[3];
-        $end_date = $row[4];
-  echo "<tr>";
-  echo "<td>$projekt_id</td>";
-  echo "<td>$projekt_name</td>";
-  echo "<td>$kategori</td>";
-  echo "<td>$start_date</td>";
-  echo "<td>$end_date</td>";
-  echo "</tr>";
-    }
-
-}
-
-function test2($mysqli){
-    
-
-
 $result = mysqli_query($mysqli,"SELECT projekt_id, projekt_name, kategori, start_date, end_date FROM projekt");
 
     echo"<table class='pure-table pure-table-striped'>
@@ -282,5 +250,4 @@ while($row = mysqli_fetch_array($result)) {
 }
 
 echo "</table>";
-
 }
