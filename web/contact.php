475b8dc7eeefa94491dfe5c05ebe891be60cd9a1
martinsoender <wxk220@ku.dk>
2014-04-30 18:11:57 +0200

    added contactform to contact + php functions

--- web/contact.php
+++ web/contact.php
@@ -13,6 +13,8 @@ sec_session_start();
 <meta name="description" content="A layout example with a side menu that hides on mobile, just like the Pure website.">
     <title>Projects</title>
 <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.4.2/pure.css">
+<link rel="stylesheet" href="css/pure.css"/>
+<link rel="stylesheet" href="css/pure-form.css" />
 
     <!--[if lte IE 8]>
         <link rel="stylesheet" href="css/layouts/side-menu-old-ie.css">
@@ -55,11 +57,34 @@ sec_session_start();
         </div>
 
         <div class="content">
-            <h2 class="content-subhead">Titel</h2>
-            <p>
-                Tekst
-            </p>
-        </div>
+            <h2 class="content-subhead"></h2>
+            
+            <form class="pure-form pure-form-stacked" method="post" action="contactengine.php">
+                <div>
+                <label for="Name"><b>Navn</b></label>
+                <input type="text" name="Name" id="Name" />
+                </div>
+                <br><br>
+                <div>
+                <label for="City"><b>By</b></label>
+                <input type="text" name="City" id="City" />
+                </div>
+                <br><br>
+                <div>
+                <label for="Email"><b>Email</b></label>
+                <input type="text" name="Email" id="Email" />
+                </div>
+                <br><br>
+                <div>
+                <label for="Message"><b>Besked</b></label>
+                <textarea type="text" name="Message" rows="5" cols="50" id="Message"></textarea>
+                </div>
+                <br><br><br><br><br>
+                <input type="submit" name="submit" value="Submit" class="btn left" />
+            </form>
+            
+            <div style="clear: both;"></div>
+        
     </div>
 </div>
 
---
+++ web/contactthanks.php
@@ -0,0 +1,32 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Strict//EN">
+
+<head>
+	<title>Projects</title>
+	
+	<link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+
+<body>
+
+	<div id="page-wrap">
+			
+			
+		<br /><br />
+		
+			
+		<h1>Din besked er blevet sendt!</h1><br />
+		
+		<p><a href="index.html">Tilbage til kontakt siden</a></p>
+	
+	</div>
+	
+	<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+	</script>
+	<script type="text/javascript">
+	_uacct = "UA-68528-29";
+	urchinTracker();
+	</script>
+
+</body>
+
+</html>
