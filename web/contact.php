f23078d321e4fddd3acc45995671bc29aedae011
martinsoender <wxk220@ku.dk>
2014-05-06 13:10:33 +0200

    forgot to add new project to contact page

--- web/contact.php
+++ web/contact.php
@@ -42,6 +42,9 @@ sec_session_start();
                 <li><a href="all_projects.php">Alle projekter</a></li>
                 <li><a href="history.php">Min historik</a></li>
                 <li><a href="contact.php">Kontakt</a></li>
+                <?php if (check_admin($mysqli) == true) : ?>
+                <li> <a href="new_project.php">Nyt projekt</a></li>
+                <?php endif; ?>
                 <br><br>
                 <li><a href="includes/logout.php">Log ud</a></li>
             </ul>
