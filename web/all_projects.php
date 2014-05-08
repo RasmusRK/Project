f49615bfd36a9d813e5525d20834e8856c863694
TheNiceOne <henrik-thomsen@live.dk>
2014-05-08 11:03:41 +0200

    small changes

--- web/all_projects.php
+++ web/all_projects.php
@@ -8,71 +8,58 @@ sec_session_start();
 <!doctype html>
 <html lang="en">
 <head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta name="description" content="A layout example with a side menu that hides on mobile, just like the Pure website.">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Projects</title>
-<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.4.2/pure.css">
-
-    <!--[if lte IE 8]>
-        <link rel="stylesheet" href="css/layouts/side-menu-old-ie.css">
-    <![endif]-->
-    <!--[if gt IE 8]><!-->
-        <link rel="stylesheet" href="css/layouts/side-menu.css">
-    <!--<![endif]-->
-  </head>
+    <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.4.2/pure.css">
+    <link rel="stylesheet" href="css/layouts/side-menu.css">
+</head>
 
 <body>
- <?php if (login_check($mysqli) == true) : ?>
-            <div id="layout">
-    <!-- Menu toggle -->
-    <a href="#menu" id="menuLink" class="menu-link">
-        <!-- Hamburger icon -->
-        <span></span>
-    </a>
+    <?php if (login_check($mysqli) == true) : ?>
+    <div id="layout">
+        <a href="#menu" id="menuLink" class="menu-link">
+            <span></span>
+        </a>
 
-    <div id="menu">
-        <div class="pure-menu pure-menu-open">
-            <a class="pure-menu-heading" align="center" href="main.php"><img src="img/logo1.png"></a>
+        <div id="menu">
+            <div class="pure-menu pure-menu-open">
+                <a class="pure-menu-heading" align="center" href="main.php"><img src="img/logo1.png"></a>
+                <ul>
+                    <li><a href="my_projects.php">Mine projekter</a></li>
+                    <li><a href="all_projects.php">Alle projekter</a></li>
+                    <li><a href="history.php">Min historik</a></li>
+                    <li><a href="contact.php">Kontakt</a></li>
+                    <?php if (check_admin($mysqli) == true) : ?>
+                        <li> <a href="new_project.php">Nyt projekt</a></li>
+                    <?php endif; ?>
+                    <br><br>
+                    <li>Logget ind som: <?php echo $_SESSION['username'];?></li>
+                    <li>Du har bruger id: <?php echo $_SESSION['user_id'];?></li>
+                    <li><a href="includes/logout.php">Log ud</a></li>
+                </ul>
+            </div>
+        </div>
 
-            <ul>
-                <li><a href="my_projects.php">Mine projekter</a></li>
-                <li><a href="all_projects.php">Alle projekter</a></li>
-                <li><a href="history.php">Min historik</a></li>
-                <li><a href="contact.php">Kontakt</a></li>
-                <?php if (check_admin($mysqli) == true) : ?>
-                <li> <a href="new_project.php">Nyt projekt</a></li>
-                <?php endif; ?>
-                <br><br>
-                <li>Logget ind som: <?php echo $_SESSION['username'];?></li>
-                <li>Du har bruger id: <?php echo $_SESSION['user_id'];?></li>
-                <li><a href="includes/logout.php">Log ud</a></li>
-            </ul>
+        <div id="main">
+            <div class="header">
+                <h1>Alle projekter</h1>
+                <h2>Liste over alle projekter</h2>
+            </div>
+
+            <div class="content">
+                <h2 class="content-subhead">Titel</h2>
+                <p> <?php show_projects($mysqli); ?> </p>
+            </div>
         </div>
     </div>
 
-    <div id="main">
-        <div class="header">
-            <h1>Alle projekter</h1>
-            <h2>Liste over alle projekter</h2>
-            
-        </div>
-
-        <div class="content">
-            <h2 class="content-subhead">Titel</h2>
-            <p>
-                <?php show_projects($mysqli); ?>
-            </p>
-        </div>
-    </div>
-</div>
-
-<script src="js/ui.js"></script>
+    <script src="js/ui.js"></script>
         <?php else : ?>
             <p>
                 <span class="error">Du har ikke rettigheder til at komme ind på siden.</span> Gå venligst tilbage til <a href="index.php">login siden</a>.
             </p>
         <?php endif; ?>
-
 </body>
+
 </html>
