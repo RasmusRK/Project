0633270fa2da4f6f6cb7fbf9e019c6c243b51f57
RasmusRK <rasmus.klitgaard.rk@gmail.com>
2014-05-29 23:12:41 +0200

    added print to main for testing

--- web/main.php
+++ web/main.php
@@ -34,6 +34,7 @@ sec_session_start();
                     <?php if (check_overadmin($mysqli) == true) : ?>
                     <li> <a href='administrator.php'>Administrator</a></li>
                     <?php endif; ?>
+                    <li> <a href ='sql_table_to_pdf/generate-pdf.php'> Print </a></li>
                     <li><a class='logout' href='includes/logout.php'>Log ud</a></li>
                 </ul>
             </div>
