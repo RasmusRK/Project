5c43aff96883d4c9da837f8c4cc2456bb7f39bf0
TheNiceOne <henrik-thomsen@live.dk>
2014-05-30 01:20:29 +0200

    træt..

--- web/administrator.php
+++ web/administrator.php
@@ -50,25 +50,20 @@ sec_session_start();
                 You are super admin, congrats! :) <br><br>
                 Du kan ændre hvilken mail adresse der skal sendes 'kontakt' til.<br>
                 <?php user_table($mysqli); ?> <br><br>
-                Vælg et id på den du ønsker, at lave til super admin.<br>
+                Vælg et id på den du ønsker, at lave til super admin.<br>   
                 Dette medføre, at du ikke kan tilgå administrator siden mere.<br>
                 (Der kan kun være en super administrator på en gang)<br>
                 <form>
                 <div>
                     <label for="id"><b>Bruger-id</b></label>
                     <input id ="id" type="int" name="id" required />
-                </div>
-                    <input class="btn right" type="submit" value="Ny admin"> 
-                    <?php
-                        $oldAdmin = $_SESSION['user_id'];
-                        $newAdmin = $_REQUEST['id'];
-                        if (get_userState($mysqli, $newAdmin) == 1 && get_userState($mysqli, $oldAdmin) == 2) {
-                            mysqli_query($mysqli,"UPDATE users SET admin = 2    
-                                                  WHERE id = $newAdmin");
-                            mysqli_query($mysqli,"UPDATE users SET admin = 1    
-                                                  WHERE id = $oldAdmin");
-
-                            mysqli_close($mysqli);
+                </div>'
+                    <input class="btn right" type="submit" value="Ny admin">
+                    <?php 
+                        if (isset($_REQUEST['id']) && $_REQUEST['id'] == 1) {
+                            $oldAdmin = $_SESSION['user_id'];
+                            $newAdmin = $_REQUEST['id'];
+                            echo "<META HTTP-EQUIV='Refresh' Content='0; URL=admin_confirmation.php?nid=$newAdmin&oid=$oldAdmin'?>";
                         }
                     ?>
                 </form>
