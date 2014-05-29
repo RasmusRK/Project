e0bea9d5bc6f7ff89bcb917cea7d4a438926a7ee
RasmusRK <rasmus.klitgaard.rk@gmail.com>
2014-05-29 23:05:14 +0200

    removed info

--- web/sql_table_to_pdf/generate-pdf.php
+++ web/sql_table_to_pdf/generate-pdf.php
@@ -21,19 +21,19 @@ mysql_select_db('projekt');
 $pdf=new PDF();
 $pdf->AddPage();
 //First table: put all columns automatically
-$pdf->Table('SELECT `project_name`, `category`,`start_date`,`end_date`,`info` from projekt order by `project_name`');
+$pdf->Table('SELECT `project_name`, `category`,`start_date`,`end_date` from projekt order by `project_name`');
 $pdf->AddPage();
 //Second table: specify 3 columns
 $pdf->AddCol('project_name',40,'','C');
 $pdf->AddCol('category',40,'projekt','C');
 $pdf->AddCol('start_date',40,'','C');
 $pdf->AddCol('end_date',40,'','C');
-$pdf->AddCol('info',40,'','C');
+//$pdf->AddCol('info',40,'','C');
 $prop=array('HeaderColor'=>array(255,150,100),
 			'color1'=>array(210,245,255),
 			'color2'=>array(255,255,210),
 			'padding'=>2);
-$pdf->Table('select project_name, category,start_date, end_date, info from projekt order by project_name limit 0,10',$prop);
+$pdf->Table('select project_name, category,start_date, end_date from projekt order by project_name limit 0,10',$prop);
 
 //$pdf->Output("C:\Users\John\Desktop/somename.pdf",'F'); 
 
