02190dcacd1162607d1f579e86304b2011606518
TheNiceOne <henrik-thomsen@live.dk>
2014-06-03 14:18:35 +0200

    added something

---
+++ web/get_password.php
@@ -0,0 +1,5 @@
+Her skal der laves et eller andet form for update statement til en bruger der har glemt sit password.
+
+
+<br><br><br>
+Jeg aner ikke hvordan vi skal gøre det, men vi skal helt sikkert bruge noget fra /includes/register.inc.php
